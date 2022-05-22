f2f5ffd7710ece3fe71b6ad45fc6484e78363fa7
andrea scacchi <andrea.scacchi@live.com>
2022-05-22 14:14:32 +0200

    create dinamically populated cards from array of data

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-[
+const data = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 // script for Exercise
+
+const mainContainer = document.querySelector("main.show");
+
+for (i=0; i<data.length; i++){
+    // create card
+    const divCard = document.createElement("div");
+    divCard.classList.add("card");
+
+    // create i element
+    let cardIcon = document.createElement("i");
+        cardIcon.classList.add("fa-solid");
+        cardIcon.classList.add(`fa-${data[i].name}`);
+    // create p element
+    let cardName = document.createElement("p");
+        cardName.innerHTML = data[i].name;
+        divCard.classList.add(data[i].color);
+
+    // assemble card
+    divCard.append(cardIcon);
+    divCard.append(cardName);
+    mainContainer.append(divCard);
+}
