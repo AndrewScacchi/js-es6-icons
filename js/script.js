2ac135446355b441be26609e23dbe8aafeee4b5a
andrea scacchi <andrea.scacchi@live.com>
2022-05-23 09:58:28 +0200

    Esercizio completato. aggiunta basica funzionalità al cambio. Nessun Bonus, esercizio già in ritardo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 // script for Exercise
-
 const mainContainer = document.querySelector("main.show");
-
+// creating arrays
 const animals = [];
 const users = [];
 const vegetables = [];
 
-// selecting all the options
-const selectedAll = document.getElementById("all");
-const selectedUsers = document.getElementById("users");
-const selectedAnimals = document.getElementById("animals");
-const selectedVegetables = document.getElementById("vegetables");
+// using change mean i need to grab the whole section
+const selector = document.getElementById("type");
+
+selector.addEventListener('change', (event) => {
+  const result = document.querySelector('.result');
+    mainContainer.innerHTML = "";   
+  if(event.target.value == "all"){  
+    // mainContainer.innerHTML = `You like ${event.target.value}`;
+    for (i=0; i<data.length; i++){
+        createCard(i,data);
+    }
+  } else if (event.target.value == "users"){
+    for (i=0; i<users.length; i++){
+        createCard(i,users);
+    }
+    // mainContainer.innerHTML = `You like ${event.target.value}`;
+  } else if (event.target.value == "animals") {
+    for (i=0; i<animals.length; i++){
+        createCard(i,animals);
+    }
+    // mainContainer.innerHTML = "buu";  
+  } else if (event.target.value == "vegetables"){
+    for (i=0; i<vegetables.length; i++){
+        createCard(i,vegetables);
+    }
+  }
+});
+// si sopra sarebbe meglio uno switch ma son più veloce a ragionare di if e l'esercizio è già in ritardo
+// inoltre si forse era meglio salvare il ciclo a sua volta in una fx ma avevo già scritto la fx come card creator singola
+
 
 //populating the other arrays
 for (i=0; i<data.length; i++){
@@ -23,26 +47,6 @@ for (i=0; i<data.length; i++){
     }
 
 }
-
-//okay one by one they work
-for (i=0; i<data.length; i++){
-    createCard(i,data);
-}
-/*
-for (i=0; i<users.length; i++){
-    createCard(i,users);
-}
-for (i=0; i<animals.length; i++){
-    createCard(i,animals);
-}
-for (i=0; i<vegetables.length; i++){
-    createCard(i,vegetables);
-}
-*/
-
-
-
-
 
 // Create a card function
 function createCard(iter, array){
@@ -66,35 +70,3 @@ function createCard(iter, array){
 
     return divCard;
 }
-
-
-/*
-
-for (i=0; i<data.length; i++){
-    // create card
-    const divCard = document.createElement("div");
-    divCard.classList.add("card");
-
-    // create i element
-    let cardIcon = document.createElement("i");
-        cardIcon.classList.add("fa-solid");
-        cardIcon.classList.add(`fa-${data[i].name}`);
-    // create p element
-    let cardName = document.createElement("p");
-        cardName.innerHTML = data[i].name;
-        divCard.classList.add(data[i].color);
-
-    // assemble card
-    divCard.append(cardIcon);
-    divCard.append(cardName);
-    mainContainer.append(divCard);
-}
-*/
-
-
-
-
-
-console.log(users);
-console.log(animals);
-console.log(vegetables);
